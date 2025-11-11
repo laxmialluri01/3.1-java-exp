442431e82b5c91a48c32aa56bc65a3edce46fa91
laxmialluri01 <laiiluru@gmail.com>
2025-11-11 10:02:45 +0530

    Create LoginServelet.java

---
+++ LoginServelet.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String user = request.getParameter("username");
+        String pass = request.getParameter("password");
+
+        if ("admin".equals(user) && "1234".equals(pass)) {
+            out.println("<h2>Welcome, " + user + "! Login Successful.</h2>");
+        } else {
+            out.println("<h2>Invalid credentials. Try again.</h2>");
+        }
+    }
+}
